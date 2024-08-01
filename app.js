5f10a721ba6ecf2b95093e82fb885a3dee9c5a6d
Youssef <--replace-all>
2024-08-01 03:15:08 +0300

    add production port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
